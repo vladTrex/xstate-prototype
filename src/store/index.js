b291235a0f5d9539e460059086fbd86d059634ed
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2022-09-05 22:17:46 +0300

    add volume state

--- src/store/index.js
+++ src/store/index.js
@@ -1,46 +1,78 @@
 import { createMachine, assign } from 'xstate';
 import { fetchMockedDataV2 } from '../api';
 
-export const playerMachine = createMachine({
-    id: 'player',
-    initial: 'loading',
-    context: {
-        count: 0,
-    },
-    states: {
-        loading: {
-            entry: ['loadData'],
-            on: {
-                SUCCESS: {
-                    actions: 'assignDataLog',
-                    target: 'playing'
-                }
+export const playerMachine =
+    /** @xstate-layout N4IgpgJg5mDOIC5QAcA2BDAnmATgOjS1z1QHt0IBLAOygGIBlAVQGEWBRBhxFU2SgC6VS1HiAAeiAEwA2ABx4AzDIDsARjkBWAJyypUuVIA0ITNJna8mgAzW1mxXJmK11zQBYAvp5OFs+P2IcMApMAgxMGnoABQBBJgZ2MWQ+QWFRJAlEGVU8bXztFUVFFW13TXkTMwRFd0U8NRynUpUKuykvHxBAgIigkIgw5HQAV1hIOmiAGViATWTUoRExSQRVS2cpdW01Hbl3UqrpLSVim00LuWsdzW8u6lIIOGS+3qJ8Mgoohf4ljNBVipjKZpJoFLY7O4DEC5HJlDJvL5XuF3nhgqEUZFaD80stMqtypZXNoSvo1O5tDIoZojggOjI8BC1C5YbIZBpEd1kT00QMhqNxhAcX8VogVCprHgVDILi5XGogSpaQZLDZrPp1dpNFJGmpOTyeejBsL0qKEHVNA1rqSdRSqVIaSCEAr3IyIXJSjrxeVOkjUT0TXiAYh3HJaa4pG7bHVKdZYUDbl0eQA3UioEYAWzAeBG1AzIwEkED-yyCCBtM0rSj9h1UkU1nkeqTyNT6azeHzhaFmRSv1N+MQcjUjIlijrmkayihtPteQKKnczNKtRU+pbaczYGLZtDFes9QhOp2ignOzra-e24HdPcyssBQfj+0TjuniAA */
+    createMachine({
+        context: { count: 0, volume: 3 },
+        id: "player",
+        type: "parallel",
+        states: {
+            player: {
+                initial: "loading",
+                states: {
+                    loading: {
+                        entry: "loadData",
+                        on: {
+                            SUCCESS: {
+                                actions: "assignDataLog",
+                                target: "ready",
+                            },
+                        },
+                    },
+                    ready: {
+                        initial: "playing",
+                        states: {
+                            playing: {
+                                entry: [
+                                    "playAudio",
+                                    assign({ count: (context) => context.count + 1 }),
+                                ],
+                                exit: "pauseAudio",
+                                on: {
+                                    PAUSE: {
+                                        target: "paused",
+                                    },
+                                },
+                            },
+                            paused: {
+                                on: {
+                                    PLAY: {
+                                        target: "playing",
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            volume: {
+                initial: "unmuted",
+                states: {
+                    unmuted: {
+                        on: {
+                            'VOLUME.TOGGLE': 'muted',
+                        },
+                    },
+                    muted: {
+                        on: {
+                            'VOLUME.TOGGLE': 'unmuted',
+                        },
+                    },
+                },
             },
         },
-        playing: {
-            entry: ['playAudio', assign({ count: context => context.count + 1 })],
-            exit: 'pauseAudio',
-            on: {
-                PAUSE: { target: 'paused' },
-            },
+    }).withConfig({
+        actions: {
+            loadData: () => console.log('📦 Configured loading data'),
+            assignDataLog: () => console.log('💾 Accessing data'),
+            playAudio: () => console.log('🎹 🎵 Playing music'),
+            pauseAudio: () => console.log('⏸️ Pausing music'),
         },
-        paused: {
-            on: {
-                PLAY: { target: 'playing' },
-            },
-        }
-    }
-}).withConfig({
-    actions: {
-        loadData: () => console.log('📦 Configured loading data'),
-        assignDataLog: () => console.log('💾 Accessing data'),
-        playAudio: () => console.log('🎹 🎵 Playing music'),
-        pauseAudio: () => console.log('⏸️ Pausing music'),
-    },
-    // guards: {
-    //     has3Times: context => context.count < 3
-    // }
-});
+        // guards: {
+        //     has3Times: context => context.count < 3
+        // }
+    });
 
 export const stepperMachine = createMachine({
     id: 'stepper_machine',
